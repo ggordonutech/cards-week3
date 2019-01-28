574d5020f5d87cc5e7d43c8077a07d8e0c087642
gggordon <ggg_gilroy@yahoo.com>
2019-01-28 15:50:06 -0500

    CardDatase readFirstRecord

--- src/main/java/CardDatabase.java
+++ src/main/java/CardDatabase.java
@@ -1,5 +1,8 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class CardDatabase 
@@ -31,8 +34,31 @@ public class CardDatabase
 	}
 
 	public Card readFirstRecord() {
-		// TODO Auto-generated method stub
-		return null;
+		Card data = null;
+		ObjectInputStream ois = null;
+		
+		try {
+			ois = new ObjectInputStream(
+					new FileInputStream(DB_NAME)
+					);
+			data = (Card) ois.readObject();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try {
+				if(ois!=null)ois.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return data;
 	}
 
 }
